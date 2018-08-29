0729be5924699cd55f6732813834ac42b3b69d9d
Rick Budzak <pyroja2kx@hotmail.com>
2018-08-28 22:30:37 -0700

    refactored string replace, may think of an escape character situation next

--- script.js
+++ script.js
@@ -3,15 +3,5 @@ console.log("Sanity check!");
 function dashToSpace(){
     console.log('d2s click');
     var textarea = document.getElementById("intext");
-    var intext = textarea.value;
-    var outtext = [];
-    for (var i = 0; i < intext.length; i++){
-        if (intext[i] == '-') {
-            outtext.push(" ");
-        }
-        else {
-            outtext.push(intext[i]);
-        }
-    }
-    textarea.value = outtext.join('');
+    textarea.value = textarea.value.replace(/-/g, " ");    
 }
